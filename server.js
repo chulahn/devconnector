f9ca74cf76d716a1e951f0b86e5a4f3a545bacf8
Chul Ahn <scahn.x@gmail.com>
2018-07-11 17:37:42 -0400

    passport jwt authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -21,11 +22,15 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+// Passport middleware
+app.use(passport.initialize());
 
-//User Routes
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
 app.use("/api/users", users);
-app.use("/ api/profile", profile);
+app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
